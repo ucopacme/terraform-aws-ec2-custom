a4ede581b6cc5ee3d234004cb832a03fe35840db
Khalid Ahmadzai <khalid.ahmadzai@ucop.edu>
2022-01-11 15:31:54 -0800

    initial commit

--- output.tf
+++ output.tf
@@ -1,17 +1,26 @@
-output "instance_id" {
-  description = "The ID of the VPC"
-  value       = module.ec2.instance_id
+output "instance_name" {
+description = "The tag name for this instance"
+value = var.tags.Name
 }
-
+output "instance_id" {
+  description = "Instance ID"
+  value       = join("", aws_instance.this.*.id)
+  
+}
 output "instance_arn" {
   description = "Instance ID"
-  value       = module.ec2.instance_arn
+  value       = join("", aws_instance.this.*.arn)
 }
 output "instance_public_ip" {
   description = "Instance ip"
-  value       = module.ec2.instance_public_ip
+  value       = join("", aws_instance.this.*.public_ip)
 }
 output "instance_private_ip" {
   description = "Instance ip"
-  value       = module.ec2.instance_private_ip
+  value       = join("", aws_instance.this.*.private_ip)
 }
+
+# output "instance_id" {
+#   description = "The ID of the VPC"
+#   value       = module.ec2.instance_id
+# }
