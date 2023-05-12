56b3ec3e32d6588363cb8c0f507567f8dc02177e
Khalid <33844176+ahmadzaikk@users.noreply.github.com>
2023-05-12 16:20:55 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -26,7 +26,7 @@ resource "aws_instance" "this" {
   user_data                   = var.user_data
   network_interface {
     network_interface_id  = var.network_interface_id
-    device_index          = 0
+    device_index          = 1
   }
   
 
